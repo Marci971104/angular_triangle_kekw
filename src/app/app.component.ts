177bf2551d65ce4d261d77044557cea30767e546
Marcell Rohrbacher <rohrbacher.marcell97@gmail.com>
2022-01-06 10:01:18 +0100

    hehe elso kulva komit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'triang';
+
+  teruletVisible=false;
+
+
+  alap = new FormControl ('');
+  magassag = new FormControl('');
+  terulet = new FormControl('');
+
+
+  onClickSzamit(){
+    let alap = Number(this.alap.value);
+    let magassag = Number(this.magassag.value);
+    let terulet = alap*magassag/2;
+    this.terulet.setValue(terulet);
+
+    console.log(terulet);
+    this.teruletVisible=true;
+    
+  }
+
 }
